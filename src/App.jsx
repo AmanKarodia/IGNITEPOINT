831834068d3eadcc63d432c0a563f73e900f695b
Aman Karodia <Amankarodia59@gmail.com>
2026-03-18 11:10:37 +0200

    removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import './App.css'
-import { Analytics } from "@vercel/analytics/next"
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
@@ -16,14 +14,12 @@ import CourseCat from './components/CourseCat';
 function App() {
   return (
     <Router>
-      <Analytics />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/Enquiry" element={<EnquiryPage />} />
         <Route path="/Contact" element={<ContactPage />} />
         <Route path="/Courses" element={<CoursePage />} />
       </Routes>
-      <SpeedInsights />
     </Router>
   );
 }
